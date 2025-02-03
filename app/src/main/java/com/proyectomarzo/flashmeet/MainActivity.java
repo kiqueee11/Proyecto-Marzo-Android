d4be647638bb007e1aca5fe91fdef15e38995c92
MarcosObiang <49719401+MarcosObiang@users.noreply.github.com>
2025-02-03 12:22:23 +0100

    commit inicial

---
+++ app/src/main/java/com/proyectomarzo/flashmeet/MainActivity.java
@@ -0,0 +1,43 @@
+package com.proyectomarzo.flashmeet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_login;
+    Button btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_login=(Button)findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==R.id.btn_login){
+            Intent intent=new Intent(this,ProfileActivity.class);
+            startActivity(intent);
+        }
+
+
+    }
+}
