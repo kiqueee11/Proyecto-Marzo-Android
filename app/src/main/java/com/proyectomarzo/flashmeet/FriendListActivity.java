f74da1b1e0a78dffda411c865ebf9624a5db837f
MarcosObiang <49719401+MarcosObiang@users.noreply.github.com>
2025-02-04 13:17:07 +0100

    Creando pantalla amigos, tema creado

---
+++ app/src/main/java/com/proyectomarzo/flashmeet/FriendListActivity.java
@@ -0,0 +1,33 @@
+package com.proyectomarzo.flashmeet;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.proyectomarzo.flashmeet.friends_recycler_view.FriendsListAdapter;
+
+public class FriendListActivity extends AppCompatActivity {
+    private static final String TAG = "FriendListActivity";
+    private FriendsListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_friend_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        RecyclerView recyclerView = findViewById(R.id.friend_list);
+        adapter = new FriendsListAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
